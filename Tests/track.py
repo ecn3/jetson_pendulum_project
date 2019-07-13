03bfe561f1c1017552e87ca23602516279f9b3e8
nvidia <ecn3@students.uwf.edu>
2019-07-13 00:17:22 +0000

    tracks two objects

--- Tests/track.py
+++ Tests/track.py
@@ -6,7 +6,8 @@ import numpy as np
 class Target:
     def __init__(self):
         self.capture = cv.VideoCapture("/dev/video1")
-     
+        
+        
     def run(self):
         #initiate font
         font = cv.FONT_HERSHEY_SIMPLEX
@@ -15,11 +16,15 @@ class Target:
         frame_width = int(self.capture.get(cv.CAP_PROP_FRAME_WIDTH))
         
         #instantiate images
+        #hsv_img=cv.CreateImage(cv.GetSize(cv.QueryFrame(self.capture)),8,3)
         hsv_img = np.zeros((frame_height, frame_width, 3), dtype=np.uint8)
         threshold_img1 = np.zeros((frame_height, frame_width, 1), dtype=np.uint8)
         threshold_img1a = np.zeros((frame_height, frame_width, 1), dtype=np.uint8)
         threshold_img2 = np.zeros((frame_height, frame_width, 1), dtype=np.uint8)
         i=0
+        
+        fourcc = cv.VideoWriter_fourcc('M','J','P','G')
+        writer=cv.VideoWriter("angle_tracking.avi",fourcc,30,(frame_height, frame_width),1)
  
         while True:
             #capture the image from the cam
@@ -27,6 +32,7 @@ class Target:
  
             #convert the image to HSV
             hsv_img = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+        
  
             #threshold the image to isolate two colors
             cv.inRange(hsv_img,(165,145,100),(250,210,160),threshold_img1) #red
@@ -38,6 +44,8 @@ class Target:
  
  
             #determine the moments of the two objects
+            #threshold_img1=cv.getMat(threshold_img1)
+            #threshold_img2=cv.getMat(threshold_img2)
             moments1=cv.moments(threshold_img1)
             moments2=cv.moments(threshold_img2)
             area1 = moments1['m00'] 
@@ -61,8 +69,8 @@ class Target:
  
             if (area2 >100000):
                 #x and y coordinates of the center of the object is found by dividing the 1,0 and 0,1 moments by the area
-                x2=int(moments2['m10']/area2)
-                y2=int(moments2['m01']/area2)
+                x2=int(moments1['m10']/area2)
+                y2=int(moments1['m01']/area2)
  
                 #draw circle
                 cv.circle(img,(x2,y2),2,(0,255,0),20)
@@ -70,7 +78,7 @@ class Target:
                 cv.putText(img,str(x2)+","+str(y2),(x2,y2+20),font, 1,(255,255,255)) #Draw the text
                 cv.line(img,(x1,y1),(x2,y2),(0,255,0),4,cv.LINE_AA)
                 #draw line and angle
-                #cv.line(img,(x1,y1),(frame_height, frame_width, y1),(100,100,100,100),4,cv.LINE_AA)
+                cv.line(img,(x1,y1),(frame_height, frame_width, y1),(100,100,100,100),4,cv.LINE_AA)
             x1=float(x1)
             y1=float(y1)
             x2=float(x2)
